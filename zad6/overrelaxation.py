4fe5a55ce1f9f62e386b2ad49bdcd7c2feeea74b
JHeczko <jakub.heczko5@gmail.com>
2023-12-28 19:05:58 +0100

    Zadanie 6 , overrelaxation optimilized

--- zad6/overrelaxation.py
+++ zad6/overrelaxation.py
@@ -13,8 +13,8 @@ error = 1e-10
 def TriDiag(a,b,c):
     return np.diag(a,-1) + np.diag(b,0) + np.diag(c,1)
 
-#OverRelaxation
-# def OverRelaxation(A,b,xn):
+#OverRelaxation No Optimalization
+# def OverRelaxationNoOptimalization(A,b,xn):
 #     xn1 = np.zeros(N)
 #     for i in range(1,N+1):
 #         sum1 = 0
@@ -25,17 +25,17 @@ def TriDiag(a,b,c):
 #             sum2 += A[i-1][j-1]*xn[j-1]
 #         xn1[i-1] = (1-gamma)*xn[i-1]+(gamma/A[i-1][i-1])*(b[i-1] - sum1 - sum2)
 #     return xn1
+
+#OverRelaxation
 def OverRelaxation(A,b,xn):
     xn1 = np.zeros(N)
     for i in range(1,N+1):
         sum1 = 0
         sum2 = 0
-        for j in range(1,i):
-            sum1 += A[i-1][j-1]*xn1[j-1]
-        for j in range(i+1,N+1):
-            if(A[i-1][j-1] == 0):
-                print(f"Przez zero: j = {j} oraz i = {i}")
-            sum2 += A[i-1][j-1]*xn[j-1]
+        if(i != 1):
+            sum1+=A[i-1][i-2]*xn1[i-2]
+        if(i != N):
+            sum2 += A[i-1][i]*xn[i]
         xn1[i-1] = (1-gamma)*xn[i-1]+(gamma/A[i-1][i-1])*(b[i-1] - sum1 - sum2)
     return xn1
 
@@ -58,13 +58,12 @@ print(np.linalg.solve(A,d))
 
 #Iteracja
 xn=np.zeros(N)
-OverRelaxation(A,d,xn)
-# while(True):
-#     xn1 = OverRelaxation(A,d,xn)
-#     if(np.abs(max(xn1) - max(xn)) < 1e-10):
-#         print(iteration)
-#         print(xn1)
-#         break
-#     else:
-#         iteration += 1
-#         xn = xn1
+while(True):
+    xn1 = OverRelaxation(A,d,xn)
+    if(np.abs(max(xn1) - max(xn)) < 1e-10):
+        print(iteration)
+        print(xn1)
+        break
+    else:
+        iteration += 1
+        xn = xn1
